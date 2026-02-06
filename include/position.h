5508407e2d2efa6754df100aca124eaa0383d7e5
“beuve” <nicolas@beuve.net>
2026-02-06 13:47:55 +0100

    Add doc to moving and removing piece from position

--- include/position.h
+++ include/position.h
@@ -42,9 +42,42 @@ Rank rank_of_square(Square sq);
 File file_of_square(Square sq);
 Square square_of_rk_fl(Rank rk, File fl);
 
+/**
+ * Move a piece from square [from] to square [to].
+ * Update the representations: mailbox, bitboards and Zhash.
+ *
+ * If a piece is present in [to], the representations
+ * are updated accordingly. Appart from that, no checks
+ * are performed.
+ */
 Piece move_piece(Position *position, Square from, Square to);
+
+/**
+ * Remove a piece from square [from].
+ * Update the mailbox, bitboards and Zhash.
+ */
 Piece remove_piece(Position *position, Square from);
+
+/**
+ * Put a piece on square [to].
+ * Update the representations: mailbox, bitboards and Zhash.
+ *
+ * If a piece is present in [to], the representations
+ * are updated accordingly. Appart from that, no checks
+ * are performed.
+ */
 Piece put_piece(Position *position, Square to, Piece piece);
+
+/**
+ * Promote a piece by removing piece from [from] and adding
+ * a piece of kind [piece_kind] to [to].
+ * Update the representations: mailbox, bitboards and Zhash.
+ *
+ * If a piece is present in [to], the representations
+ * are updated accordingly. Appart from that, no checks
+ * are performed. Notably, no checks verify that the piece
+ * in [from] is a Pawn.
+ */
 Piece promote_piece(Position *position, Square from, Square to,
                     PieceKind piece_kind);
 
